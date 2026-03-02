6102cf22740b4b1c030204b306fa89aeac170e72
Stephen Zalalas <stephenzalalas@proton.me>
2026-03-02 17:21:10 -0500

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import {Routes, Route} from "react-router";
+import Home from "./Pages/Home/Home.jsx";
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </>
   )
 }
